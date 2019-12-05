1bf59fce415c60559fe6701ba08db890379afb77
vicyas96 <38896324+vicyas96@users.noreply.github.com>
2019-12-05 17:30:46 -0500

    addded new car properties and methods

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: ['seat1', 'seat2', 'seat3', 'seat4'],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log('Turn car ' + isOn);
+				if(isOn == true){
+					this.isTurnedOn = true;
+				}else{
+					this.isTurnedOn = false;
+				}
+			}
+		};
+		console.log('Hello there');
