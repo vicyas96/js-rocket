fae0096f7f87b7243f4f2fe72b9fe5540fac3b91
vicyas96 <38896324+vicyas96@users.noreply.github.com>
2019-12-07 11:32:13 -0500

    Finalizd the rocket animation and overall css

--- js/rocket.js
+++ js/rocket.js
@@ -1,27 +1,43 @@
-var timer = null;
-var countdownNumber = 5;
+let timer = null;
+let countdownNumber = 10;
 
-var changeState = function (state) {
+let changeState = function (state) {
 	document.body.className = 'body-state' + state;
 	clearInterval(timer);
-	countdownNumber = 5;
+	countdownNumber = 10;
 	document.getElementById('countdown').innerHTML = countdownNumber;
 
 	if (state == 2){
 		timer = setInterval(function () {
+
 			countdownNumber--;
 			document.getElementById('countdown').innerHTML = countdownNumber;
+			if(countdownNumber > 4 && countdownNumber <= 7){
+				document.getElementById('nervous').className = 'nervous show';
+			}else{
+				document.getElementById('nervous').className = 'nervous';
+
+			}
+
+			if(countdownNumber > 1 && countdownNumber <= 4){
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			}else{
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+
 			if(countdownNumber <= 0){
 				changeState(3);
 			}
 		}, 500);
 	}else if (state == 3){
-		var success = setTimeout(function () {
-			var randomNumber = Math.round(Math.random() * 10);
+		let success = setTimeout(function () {
+			let randomNumber = Math.round(Math.random() * 10);
 			console.log(randomNumber);
-			if(randomNumber > 5){
+			if(randomNumber > 3){
+				//success
 				changeState(4);
 			}else{
+				//fail
 				changeState(5);
 			}
 		}, 2000)
